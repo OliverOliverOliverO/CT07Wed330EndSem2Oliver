dc5f40dfe233615a73a8900bf840c4c2b622dae8
Oliver <olliken17@gmail.com>
2025-10-22 07:49:30 +0000

    q1

--- testquestion1.js
+++ testquestion1.js
@@ -1,1 +1,18 @@
+let bird
+let background, birdImg;
 
+function preload(){
+    birdImg = loadImage('assets/bird.png');
+    background = loadImage('assets/background.png');
+}
+
+function setup(){
+    new Canvas(400, 600);
+    bird = new Sprite(200, 300, 40, 40, 'static')
+    bird.image = birdImg;
+    // write code to create sprite and load bird image on sprite
+}
+
+function draw(){
+    image(background, 0, 0, width, height);
+}
