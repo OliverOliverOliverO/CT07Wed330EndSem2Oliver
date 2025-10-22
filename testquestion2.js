8b76d4efe5eec1418c61e22c615cdae27adccfd4
Oliver <olliken17@gmail.com>
2025-10-22 07:54:01 +0000

    2

--- testquestion2.js
+++ testquestion2.js
@@ -1,1 +1,18 @@
+let square;
 
+function setup(){
+    new Canvas(400, 600);
+    world.gravity.y = 10;
+
+    square = new Sprite(200, 300, 40, 40);
+
+    // code to create a new square sprite
+}
+
+function draw(){
+    background("grey");
+
+    if(mouse.presses('left')){
+        square.vel.y = -7;
+    }
+}
